32e4bafb6ee4cd9a809f8aa769beea2e1eb39dd2
muralis <muralis1223@gmail.com>
2022-08-30 14:16:05 +0530

    Adding changes

--- TubeDownload.py
+++ TubeDownload.py
@@ -12,11 +12,25 @@ class DownloadYouTube:
             print(error)
 
     def all_available_formats(self):
+        """
+        This will return all the available formats to user, so user can select which one to download.
+        :return:
+        """
         return [video.resolution for video in self.formats]
 
     def download_video(self, required_format: str = "720p"):
+        """
+        This method does download the file based on the format that user has provided,
+        by default it will try to download 720p.
+        :param required_format:
+        :return:
+        """
         self.formats.get_by_resolution(required_format).download()
 
     def download_audio_only(self):
+        """
+        This method will download the Audio only file.
+        :return:
+        """
         self.yt.streams.filter(only_audio=True).desc().first().download()
 
